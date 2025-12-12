07d3cf00b3d4e2939d9dbbb8e5bc338d68e234b4
Sultan Zacky Hermawan <sultanzacky318@gmail.com>
2025-12-12 10:58:25 +0700

    Template Dashboard Admin

--- frontend/vue-project/src/router/index.js
+++ frontend/vue-project/src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/LoginPage.vue'
+import AdminLayouts from '@/layouts/AdminLayouts.vue'
+import DashboardView from '@/views/admin/DashboardView.vue'
+import EmergencyBloodView from '@/views/admin/EmergencyBloodView.vue'
+import DonorReportView from '@/views/admin/DonorReportView.vue'
+import SubmissionReportView from '@/views/admin/SubmissionReportView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +24,33 @@ const router = createRouter({
       name: 'daftarakun',
       component: () => import('../views/RegisterPage.vue'),
     },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminLayouts,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'admin-dashboard',
+          component: DashboardView
+        },
+        {
+          path: 'darah-darurat',
+          name: 'darah-darurat',
+          component: EmergencyBloodView
+        },
+        {
+          path: 'laporan-donor',
+          name: 'laporan-donor',
+          component: DonorReportView
+        },
+        {
+          path: 'laporan-pengajuan',
+          name: 'laporan-pengajuan',
+          component: SubmissionReportView
+        },
+      ]
+    }
   ],
 })
 
---
+++ frontend/vue-project/src/views/admin/DashboardView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <section class="bg-white p-6 min-h-screen">
+        <h1 class="text-2xl font-semibold">Dashboard</h1>
+    </section>
+</template>
