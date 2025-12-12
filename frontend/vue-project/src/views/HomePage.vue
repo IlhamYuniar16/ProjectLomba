8328675326d377f43224b2ca79588edd054e0341
Nama Anda <email@anda.com>
2025-12-12 09:07:16 +0700

    Update Design dan Menyambungkan Register1

--- frontend/vue-project/src/views/HomePage.vue
+++ frontend/vue-project/src/views/HomePage.vue
@@ -1,9 +1,34 @@
 <template>
-    <section class="">
-        <div class="flex items-center gap-4">
-            <img class="w-13" src="../assets/img/logoyogyalife.png" alt="">
-            <h1 class="text-3xl text-primary">Yogyalife</h1>
-        </div>
+    <section class="bg-secondary">
+        <main class="flex justify-around items-center">
+            <div class="flex items-center gap-4">
+                <img class="w-13" src="../assets/img/logoyogyalife.png" alt="">
+                <h1 class="text-3xl text-primary">Yogyalife</h1>
+            </div>
+            <main class="gap-10 flex items-center">
+                <a href="/masuk" class="bg-primary px-5 py-2 rounded-full text-bgColor">Masuk</a>
+                <a href="/daftarakun" class="bg-bgColor px-5 py-2 rounded-full text-primary">Daftar</a>
+            </main>
+        </main>
+        <main class="bg-bgColor">
+            <main class="bg-bgColor flex justify-evenly container m-auto">
+                <a href="" class="hover:bg-primary py-5 px-5 hover:text-bgColor font-light">Beranda</a>
+                <a href="" class="hover:bg-primary py-5 px-5 hover:text-bgColor font-light">Tentang</a>
+                <a href="" class="hover:bg-primary py-5 px-5 hover:text-bgColor font-light">Permohonan</a>
+                <a href="" class="hover:bg-primary py-5 px-5 hover:text-bgColor font-light">History</a>
+            </main>
+        </main>
+        <main class="bg-secondary">
+            <main class="">
+                <div class="">
+                    <h1 class="text-primary font-light">Anda membutuhkan darah?</h1>
+                    <span class="">Silakan kirim informasi kebutuhannya di sini</span>
+                </div>
+                <div class="">
+                    <button></button>
+                </div>
+            </main>
+        </main>
     </section>
 </template>
 
